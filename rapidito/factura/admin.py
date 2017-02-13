7bb4edc3ac1e49e3581edceaebf745f42b6287ea
Daniel Saguez Tezanos Pinto <danielstp@gmail.com>
2017-02-13 18:43:28 -0400

    Modelos en el admin

--- rapidito/factura/admin.py
+++ rapidito/factura/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Empresa
+from .models import Autorización
+from .models import Factura
+
+admin.site.register(Empresa)
+admin.site.register(Autorización)
+admin.site.register(Factura)

3e775a0df2c375e3add5857b1410325e5be708cf
Henry Castro <hcvcastro@gmail.com>
2017-03-25 10:06:20 -0400

    flake8: fix "E261 at least two spaces before inline comment"

--- rapidito/users/tests/factories.py
+++ rapidito/users/tests/factories.py
@@ -1,11 +1,11 @@
 import factory
 
 
-class UserFactory(factory.django.DjangoModelFactory): # pylint: disable=too-few-public-methods
-    username = factory.Sequence(lambda n: 'user-{0}'.format(n)) # pylint: disable=unnecessary-lambda
-    email = factory.Sequence(lambda n: 'user-{0}@example.com'.format(n)) # pylint: disable=unnecessary-lambda
+class UserFactory(factory.django.DjangoModelFactory):  # pylint: disable=too-few-public-methods
+    username = factory.Sequence(lambda n: 'user-{0}'.format(n))  # pylint: disable=unnecessary-lambda
+    email = factory.Sequence(lambda n: 'user-{0}@example.com'.format(n))  # pylint: disable=unnecessary-lambda
     password = factory.PostGenerationMethodCall('set_password', 'password')
 
-    class Meta: # pylint: disable=too-few-public-methods
+    class Meta:  # pylint: disable=too-few-public-methods
         model = 'users.User'
         django_get_or_create = ('username', )
